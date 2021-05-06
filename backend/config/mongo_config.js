45e2628b4bd0da35e9121a830fb8d739c3431d0d
Shubham <shubhamdutta1411@gmail.com>
2021-05-06 01:28:15 -0700

    Change

--- backend/config/mongo_config.js
+++ backend/config/mongo_config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const config = {};
@@ -25,22 +24,3 @@ const InitiateMongoServer = async () => {
 };
 
 module.exports = InitiateMongoServer;
-=======
-const MongoClient = require('mongodb').MongoClient
- 
-// Note: A production application should not expose database credentials in plain text.
-const MONGO_URI = "mongodb+srv://admin:admin123@redditcluster.hg7v9.mongodb.net/reddit_db?retryWrites=true&w=majority"
-//const MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
- 
-function connect(url) {
-  return MongoClient.connect(url).then(client => client.db())
-}
- 
-module.exports = async function() {
-  let databases = await Promise.all([connect(MONGO_URI)])
- 
-  return {
-    production: databases[0],
-  }
-}
->>>>>>> af8a5551c08dd5f45bcda49e4e10bf0e74a6d735
