40c4b9fd1e8f60774854e9d829ee7035caf516d4
Ujjwal <ujjwal.jain1012@gmail.com>
2021-05-06 23:54:52 -0700

    implemented redis and mongo connection pool

--- backend/routes/message_routes.js
+++ backend/routes/message_routes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { checkAuth } = require("../config/passport");
 const messageController = require("../controllers/message_controller");
-
+const dummyController = require("../controllers/dummy_controller")
 // Initiate Chat
 router.get("/initiatechat", messageController.getChats);
 
 // Post Chat
 router.post("/postchat", messageController.postChats);
+
+// Presentation Route. Delete later along with dummy_controller.js
+router.post("/insert10000", dummyController.insertChats)
+router.post("/fetchAll", dummyController.fetchChats)
+router.post("/fetchCached", dummyController.fetchChatsCached)
 
 module.exports = router;
