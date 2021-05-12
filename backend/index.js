3c1540b5fec62b81e89142059a3f0bf0380a8fa3
pavanbhatt7 <pavanbhatt14@gmail.com>
2021-05-12 11:05:02 -0700

    mongodb user signup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const passport = require('passport');
+const passport = require("passport");
 const cors = require("cors");
 
 var mysqlConfig = require("./config/mysql_config");
-const mongoConfig = require('./config/util').mongoURI;
-const chat = require("./routes/chat")
+const mongoConfig = require("./config/util").mongoURI;
+const chat = require("./routes/chat");
 const user = require("./routes/user_routes");
 const post = require("./routes/post_routes");
 const bodyParser = require("body-parser");
@@ -17,17 +17,16 @@ mongoose.set("useCreateIndex", true);
 InitiateMongoServer();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 //passport middleware
 app.use(passport.initialize());
 
 app.use(
   bodyParser.urlencoded({
-    extended : true
+    extended: true,
   })
 );
 app.use(bodyParser.json());
-
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -46,12 +45,10 @@ app.use(function (req, res, next) {
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
-
-
 var options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex:true,
+  useCreateIndex: true,
   poolSize: 10,
   bufferMaxEntries: 0,
 };
@@ -66,29 +63,27 @@ const mongoConnection = async () => {
 };
 mongoConnection();
 
-
-
 app.use(cors());
 app.listen(3001, (req, res) => {
   console.log("server running on port 3001....");
 });
-const user = require("./routes/user_routes");
+
 const community = require("./routes/community_routes");
-const post = require("./routes/post_routes");
+
 //const comment = require("./routes/comment_routes");
 const message = require("./routes/message_routes");
 
 // User Routes
 app.use("/api/user", user);
-app.use("/api/chat",chat);
+app.use("/api/chat", chat);
 //Community Routes
 app.use("/api/community", community);
 //Post Routes
 app.use("/api/posts", post);
 //Comment Routescls
 //app.use("/api/comment", comment);
-app.listen(3001, (req, res) => {
-  console.log("server running on port 3001....");
-});
+// app.listen(3001, (req, res) => {
+//   console.log("server running on port 3001....");
+// });
 //Message Routes
 app.use("/api/message", message);
