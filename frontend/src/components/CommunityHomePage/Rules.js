fe261e75cb7bc25c968f9ae59e9baba6500738e2
vagdevij52 <vagdevi.junnuri@sjsu.edu>
2021-05-14 13:25:43 -0700

    pagination/sort posts

--- frontend/src/components/CommunityHomePage/Rules.js
+++ frontend/src/components/CommunityHomePage/Rules.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 export default function Rules({commRules}) {
+
+    // for(var i=0;i<commRules.length;i++){
+    //     return(
+    //         <div style={{marginLeft:"10px",background:"#FFFFFF"}}>
+    //         {commRules[i]}
+    //         </div>
+    //     )
+    // }
     return (
         <>
         <div style={{background:"#0079D3",color:"#FFFFFF",fontWeight:"bold",height:"30px"}}>
            <span style={{marginLeft:"10px"}}>Rules</span>
         </div>
         <div style={{marginLeft:"10px",background:"#FFFFFF"}}>
-            {commRules}
+            {commRules}{' '}
         </div>
         </>
     )
